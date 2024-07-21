8fee380f0c835f45b81884268c153a542c29499e
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-21 18:15:31 +0300

    handle overflow, add comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,16 @@
 #include <Serial.h>
 #include <CmdParser.h>
 
-#define INPUT_BUFFER_MAX 308 // 307 + /0
+#define INPUT_BUFFER_MAX 309 // 307 + /r
 
 // System clock by default is provided by HSI = 16MHz. Changing this will generate problems in all peripherals which use AHB, APBx bus.
 
+//Response for 3000char overflow with -O0 is 260ms with 115200b/s.
+//Theoretical end of Rx is 26ms. 
+
 // Global variables
 volatile bool inputReady = false;
+volatile bool overflow = false;
 
 char inputBuffer[INPUT_BUFFER_MAX];
 int inputLen = 0;
@@ -33,10 +37,17 @@ int main()
     while (true)
     {
         inputReady = false;
-        // Wait until rx serial port is idle
         while (!inputReady)
             ;
 
+        // Overflow response
+        if (overflow)
+        {
+            serial.printString("ERROR\r\n");
+            overflow = false;
+            continue;
+        }
+        // Command response
         if (cmdParser.readCommand() == 0)
         {
             serial.printString("OK\r\n");
@@ -63,6 +74,16 @@ extern "C" void USART2_IRQHandler(void)
         inputBuffer[readBytes] = USART2->DR;
         readBytes++;
         inputLen++;
+
+        // Overflow
+        if (readBytes >= INPUT_BUFFER_MAX)
+        {
+            // TODO: process input buffer overflow
+            overflow = true;
+            readBytes = 0;
+            inputLen = 0;
+            inputReady = false;
+        }
     }
     if (READ_BIT(USART2->SR, USART_SR_IDLE))
     {
@@ -70,5 +91,13 @@ extern "C" void USART2_IRQHandler(void)
         readBytes = 0;
         inputReady = true;
         int temp = USART2->DR;
+
+        // When receiving is over and there was overflow, reset overflow in main, 
+        //input lenght and inputready here.
+        if (overflow == true)
+        {
+            inputLen = 0;
+            inputReady = true; // input is not valid to return error
+        }
     }
 }
