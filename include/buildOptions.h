7ab2e0aa4b6622eb65c23ac1f1007e3b29fd709a
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-07-22 23:31:51 +0300

    add buildoptions

---
+++ include/buildOptions.h
@@ -0,0 +1,29 @@
+#ifndef BUILDOPTIONS_H
+#define BUILDOPTIONS_H
+
+//Baudrate
+#ifndef BAUDRATE
+#define BAUDRATE 115200
+#endif
+
+//LED 1 pin
+#ifndef LED1PIN
+#define LED1PIN "PC4"
+#endif
+
+//LED 1 pin
+#ifndef LED2PIN
+#define LED2PIN "PB13"
+#endif
+
+//LED 3 pin
+#ifndef LED3PIN
+#define LED3PIN "PB14"
+#endif
+
+//LED 4 pin
+#ifndef LED4PIN
+#define LED4PIN "PB15"
+#endif
+
+#endif // BUILDOPTIONS_H
